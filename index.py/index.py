5971598c3dc9f1502c65cb601d67d131f571080a
Thabang Kganana <thabangx12@gmail.com>
2023-09-27 17:31:38 +0200

    visual journey

---
+++ index.py/index.py
@@ -0,0 +1,52 @@
+import mysql.connector
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def mysql_data():
+    try:
+        connection = mysql.connector.connect(
+            host='bw59sqe4chjztk4k1mri-mysql.services.clever-cloud.com',
+            user='uemwk5cbfvpokdi0',
+            password='qAiIhXVCUKdob2tTzO9F',
+            database='bw59sqe4chjztk4k1mri'
+        )
+        if connection.is_connected():
+            return connection
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return None
+
+def impress_data(connection):
+    query = "SELECT Date, Impressions, Engagements FROM posts"
+    try:
+        data = pd.read_sql_query(query, connection)
+        return data
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return None
+
+def visualize(data):
+    plt.figure(figsize=(12, 6))
+    plt.bar(data['Date'], data['Impressions'], label='Impressions')
+    plt.bar(data['Date'], data['Engagements'], label='Engagements', alpha=0.7)
+    plt.xlabel('Date')
+    plt.ylabel('Count')
+    plt.title('LinkedIn Impressions and Engagements Over Time')
+    plt.xticks(rotation=45)
+    plt.legend()
+    plt.grid(axis='y')
+    plt.tight_layout()
+    plt.show()
+
+def main():
+    connection = mysql_data()
+    if connection:
+        data = impress_data(connection)
+        if data is not None:
+            visualize(data)
+        connection.close()
+
+if __name__ == "__main__":
+    main()
+
